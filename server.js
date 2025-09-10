24d1e74c4a0b62e458a450abf027148af0a83526
MirDraco <furrymir09875@gmail.com>
2025-09-10 14:07:46 +0900

    left page update

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`서버가 http://localhost:${port} 에서 실행 중입니다`);
+});
