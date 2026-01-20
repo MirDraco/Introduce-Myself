87d154c2d9a9ab00ecf586316fef591ae735e16f
Mir_Mac <mirdraco@Mir-Macui-MacBookAir.local>
2026-01-21 02:48:26 +0900

    blog page update

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const compression = require('compression');
 const app = express();
-const port = 5000;
+const port = 3000;
+
+app.use(compression()); // 데이터 압축 미들웨어 적용
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/html/index.html');
+});
+
+// 404 에러 처리 (맨 마지막에 위치해야 함)
+app.use((req, res, next) => {
+  res.status(404).send('<h1>404 Not Found</h1><p>페이지를 찾을 수 없습니다.</p>');
 });
 
 app.listen(port, () => {
